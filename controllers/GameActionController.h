fdc3401fcab92e7823d5602d9dbd3f94e88e947a
Miguel Angel Julian <miguel.a.j82@gmail.com>
2016-08-09 16:47:49 +0200

    Game representation in GameActionView

--- controllers/GameActionController.h
+++ controllers/GameActionController.h
@@ -2,16 +2,52 @@
 #define KLONDIKE_GAMEACTIONCONTROLLER_H
 
 #include "OperationController.h"
+#include "PlayerController.h"
+#include "../models/Game.h"
 
 namespace controllers
 {
     class GameActionController : public OperationController
     {
     public:
-        virtual void giveUpGame() = 0;
-        virtual void undoMovement() = 0;
-        virtual void redoMovement() = 0;
-        virtual void saveGame() = 0;
+        GameActionController(PlayerController &playerController, Game &game)
+        : playerController_(playerController), game_(game)
+                {
+                }
+        void accept(OperationControllerVisitor& operationControllerVisitor)
+        {
+            operationControllerVisitor.visit(*this);
+        }
+
+        void giveUpGame()
+        {
+            playerController_.giveUpGame();
+        }
+        void undoMovement()
+        {
+            playerController_.undoMovement();
+        }
+        void redoMovement()
+        {
+            playerController_.redoMovement();
+        }
+        void saveGame()
+        {
+            playerController_.saveGame();
+        }
+        void executeKlondikeCommand(unsigned int action)
+        {
+            playerController_.executeKlondikeCommand(action);
+        }
+        Game& getGame() const
+        {
+            return game_;
+        }
+
+    protected:
+    private:
+        PlayerController& playerController_;
+        Game& game_;
     };
 }
 
