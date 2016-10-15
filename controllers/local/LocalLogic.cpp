aa6a4b287d9c4b6bac5492eecc691cd36a11ad37
Miguel Angel Julian <miguel.a.j82@gmail.com>
2016-10-15 13:50:09 +0200

    Organize the game controllers state machine

--- controllers/local/LocalLogic.cpp
+++ controllers/local/LocalLogic.cpp
@@ -5,9 +5,9 @@
 #include "UserActionListController.h"
 
 
-controllers::local::LocalLogic::LocalLogic() : playerType_(UNINITIALIZED), deckController_(nullptr), gameActionsController_(nullptr),
-               loadGameController_(nullptr), abandonController_(nullptr), exitController_(nullptr), scoreController_(nullptr),
-               game_(nullptr), abruptExit_(false)
+controllers::local::LocalLogic::LocalLogic() : gameState_(PLAYER_NOT_SELECTED), playerType_(UNINITIALIZED), deckController_(nullptr), gameActionsController_(nullptr),
+               loadGameController_(nullptr), startGameController_(nullptr), abandonController_(nullptr),
+               exitController_(nullptr), game_(nullptr), abruptExit_(false)
 {
     playerChooseController_ = PlayerChooseControllerBuilder(*this).getPlayerChooseController();
     exitController_ = std::make_shared<controllers::local::UserActionListController>();
@@ -19,26 +19,29 @@ std::shared_ptr<controllers::OperationController> controllers::local::LocalLogic
 {
     if(abruptExit_)
         return nullptr;
-    else if(UNINITIALIZED == playerType_)
-        return playerChooseController_;
-    else if(!game_)
-    {
-        if (!loadGameController_)
-            return deckController_;
-        else
-            return loadGameController_;
-    }
-    else if(!game_->isFinished())
-        return gameActionsController_;
-    else if (!scoreController_)
-    {
-        scoreController_ = std::make_shared<controllers::local::LocalScoreController>(game_);
-        return scoreController_;
-    }
     else
     {
-        scoreController_ = nullptr;
-        return exitController_;
+        switch (gameState_)
+        {
+            case PLAYER_NOT_SELECTED:
+                return playerChooseController_;
+            case GAME_NOT_STARTED:
+                return nullptr; //loadOrNew
+            case GAME_NEW:
+                return deckController_;
+            case GAME_LOAD:
+                return loadGameController_;
+            case GAME_STARTED:
+                if(!game_->isFinished())
+                    return gameActionsController_;
+                else
+                {
+                    gameState_ = GAME_FINISHED;
+                    return std::make_shared<controllers::local::LocalScoreController>(game_);
+                }
+            case GAME_FINISHED:
+                return exitController_;
+        }
     }
 }
 
@@ -46,24 +49,28 @@ void controllers::local::LocalLogic::setPlayer(PlayerType playerType)
 {
     playerType_ = playerType;
     deckController_ = DeckControllerBuilder(*this, playerType_).getDeckController();
+    gameState_ = playerType == USER ? GAME_NEW : GAME_NEW;
     //loadGameController_ = LoadGameControllerBuilder(*playerController_, *this).getGameControllerBuilder():
 }
 void controllers::local::LocalLogic::setDeck(std::string deckPath)
 {
     game_ = std::make_shared<Game>(GameDeck(deckPath));
     gameActionsController_ = GameActionsControllerBuilder(*game_, *this).getGameActionsController(playerType_);
+    gameState_ = GAME_STARTED;
 }
 void controllers::local::LocalLogic::exitGame()
 {
     playerType_ = UNINITIALIZED;
     game_.reset();
     abruptExit_ = true;
+    gameState_ = PLAYER_NOT_SELECTED;
 }
 void controllers::local::LocalLogic::abandonGame()
 {
     playerType_ = UNINITIALIZED;
     game_.reset();
     abruptExit_ = false;
+    gameState_ = PLAYER_NOT_SELECTED;
 }
 
 void controllers::local::LocalLogic::setRandomNumberGeneratorSeed(unsigned int seed)
