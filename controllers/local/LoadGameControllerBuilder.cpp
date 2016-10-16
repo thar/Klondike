7dd8518221b97e480d6e82a546ef9bb73b568e09
Miguel Angel Julian <miguel.a.j82@gmail.com>
2016-10-16 20:56:06 +0200

    Solve the case when load is asked and no saved games exist

--- controllers/local/LoadGameControllerBuilder.cpp
+++ controllers/local/LoadGameControllerBuilder.cpp
@@ -13,10 +13,12 @@ controllers::local::LoadGameControllerBuilder::LoadGameControllerBuilder(control
 
 std::shared_ptr<controllers::ActionListController> controllers::local::LoadGameControllerBuilder::getLoadController()
 {
+    auto existingFiles = globVector("savedGames/*");
+    if (existingFiles.size() == 0)
+        return nullptr;
     std::shared_ptr<controllers::ActionListController> loadController =
             std::make_shared<controllers::local::UserActionListController>();
     loadController->setHeader(std::make_shared<StringActionListHeader>(Localization::getInstance().getValue(localization::CHOOSE_FILE)));
-    auto existingFiles = globVector("savedGames/*");
     for (auto& file : existingFiles)
     {
         loadController->addAction(std::make_shared<GameFromFileAction>(file, gameController_));
